4291e9d7399da97a3b0eedf9c77fec016f6701c1
Kn1ght16 <Danchenko0@mail.ru>
2023-07-28 01:26:31 +0300

    0.25.2.1

--- courses/views.py
+++ courses/views.py
@@ -6,33 +6,44 @@ from .serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 from users.premissions import IsOwnerOrReadOnly, IsModerator
 
 
+class CustomPagination(PageNumberPagination):
+    page_size = 10  # Количество объектов на одной странице
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    pagination_class = CustomPagination
 
 
 class CourseListCreateView(generics.ListCreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = CustomPagination
 
 
 class CourseRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    pagination_class = CustomPagination
 
 
 class LessonListCreateView(generics.ListCreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = CustomPagination
 
 
 class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly, IsModerator]
+    pagination_class = CustomPagination
 
 
 class PaymentListView(generics.ListAPIView):
@@ -41,3 +52,4 @@ class PaymentListView(generics.ListAPIView):
     filter_backends = [filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['payment_date']
     search_fields = ['paid_course_or_lesson__title', 'user__email', 'payment_method']
+    pagination_class = CustomPagination
